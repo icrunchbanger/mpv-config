eea89a78af3c6ecd3cbabd4bc9103b89ae699263
icrunchbanger <icrunchbanger@gmail.com>
2017-12-01 22:47:19 +0100

    add subdl based script for mpv since subliminal is apparently

---
+++ scripts/subdl.lua
@@ -0,0 +1,33 @@
+-- mpv script to download subtitles using subdl.
+-- requires subdl (2016 Sep master branch): https://github.com/alexanderwink/subdl
+
+-- default keybinding: B  (capital B)
+-- add the following to your input.conf to change the default keybinding:
+-- keyname script_binding subdl_load_subs
+
+local utils = require 'mp.utils'
+
+function subdl_load()
+    mp.msg.info("Searching subtitles...")
+    mp.osd_message("Searching subtitles...")
+
+    subdl = "/usr/bin/subdl" -- use 'which subdl' to find the path.
+    language = "eng"
+    download = "best-rating" --  Other options: first, most-downloaded
+
+    t = {}
+    t.args = {subdl, "--lang=" .. language, "--download=" .. download, mp.get_property("path")}
+
+    res = utils.subprocess(t)
+
+    if res.status == 0 then
+        mp.commandv("rescan_external_files", "reselect") 
+        mp.msg.info("Subtitle download succeeded.")
+        mp.osd_message("Subtitle download succeeded.")
+    else
+        mp.msg.warn("Subtitle download failed!")
+        mp.osd_message("Subtitle download failed!")
+    end
+end
+
+mp.add_key_binding("B", "subdl_load_subs", subdl_load)
